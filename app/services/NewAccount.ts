5ae272d162ac4cf23fb48bba289c1fb456ed6e1f
hiagoCf19 <hiagoferreira@id.uff.br>
2024-10-13 03:48:35 -0300

    fix: error messages when registering new account

--- app/services/NewAccount.ts
+++ app/services/NewAccount.ts
@@ -1,22 +1,39 @@
+import axios, { AxiosError } from "axios";
+import api from "./apiService";
+import { ErrorCode } from "../errors/ErrorsEnum";
+
 interface FetchNewAccountType {
   email: string;
   user_name: string;
   password: string;
 }
-export const fetchNewAccount = async ({
+interface ValidationError {
+  message: string;
+  errors: Record<string, string[]>;
+}
+export const createAccount = async ({
   email,
   user_name,
   password,
 }: FetchNewAccountType) => {
-  const serverUrl = "https://fullcar-backend.onrender.com/account/create";
   const requestData = { email, user_name, password };
 
-  const response = await fetch(serverUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(requestData),
-  });
-  return response;
+  try {
+    const response = await api.post("account/create", requestData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response?.data.message === "Account already exists") {
+        throw new Error(ErrorCode.ACCOUNT_ALREADY_EXIST);
+      }
+      if (error.code === "ERR_NETWORK") {
+        throw new Error(ErrorCode.CONNECTION_API_ERROR);
+      }
+    }
+    throw error;
+  }
 };
